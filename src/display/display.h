d50211d4942737ae1a7b2a679ce988cc996a2ffa
rahulnarayanan03 <rahulnarayanan5@outlook.com>
2026-04-01 09:16:09 +1100

    display info - Trial 1

--- src/display/display.h
+++ src/display/display.h
@@ -1,10 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <Arduino.h>
 #include <TFT_eSPI.h>
 
 class Display {
-
 
 public:
 
@@ -16,6 +16,9 @@ public:
 
 private:
     TFT_eSPI tft;
+
+    // track last-drawn step count so we only redraw when it changes
+    int _lastSteps = -1;
 
     void clearArea(int x, int y, int w, int h);
 };
