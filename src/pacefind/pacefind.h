058b71283862b168a3f51d8187189a776b6b0047
Tomarch68 <xuankien.nguyen@student.uts.edu.au>
2026-04-20 21:11:23 +1000

    Update pacefind.h
    Joe

--- src/pacefind/pacefind.h
+++ src/pacefind/pacefind.h
@@ -1,18 +1,29 @@
 #ifndef PACEFIND_H
 #define PACEFIND_H
 
+#define PACEFIND_SMOOTHING_WINDOW 4
+
 class PACEFIND {
 public:
     void update(unsigned long currentTime);
     const char* getPace();
 
-
 private:
+    static const unsigned long MIN_STEP_INTERVAL = 200;
+    static const unsigned long MAX_STEP_INTERVAL = 2000;
+    static const unsigned int  WALK_SPM_MIN      = 70;
+    static const unsigned int  RUN_SPM_MIN        = 130;
+
     unsigned long lastStepTime = 0;
-    const char* pace = "STANDING"; //default 
+    unsigned long stepIntervals[PACEFIND_SMOOTHING_WINDOW] = {};
+    int           intervalCount  = 0;   // number of valid entries in stepIntervals
+    int           intervalHead   = 0;   // circular buffer write index
+
+    const char*   currentPace = "STANDING";
+    const char*   stablePace  = "STANDING";
+    const char*   pendingPace = "STANDING"; // for anti-flicker
 
 protected:
-
 };
 
 #endif
