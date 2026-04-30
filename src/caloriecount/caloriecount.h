7a2fc1348448e2554a771c35ffd1d211239573f1
Vyasa Pinnamaraju <kp2005.aus@gmail.com>
2026-04-30 21:59:16 +1000

    calorie testing basic

--- src/caloriecount/caloriecount.h
+++ src/caloriecount/caloriecount.h
@@ -5,7 +5,25 @@
 class Caloriecount {
 public:
 
+  float getBPM;
+  float getPace;
+  float askWeight;
+  float askHeight;
 
+  int getSteps;
+  int askAge;
+
+  void userdatagather();
+  void begin();
+
+private:
+
+  bool ageAsked = false;
+  bool weightAsked = false;
+  bool heightAsked = false;
+  bool questionsAsked = false;
+  unsigned long currentTime = 0;
+  unsigned long pastTime = 0;
 
 };
 
