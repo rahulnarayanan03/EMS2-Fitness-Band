8b723e1ef26bc79010fdea9cd1ee1131d2dd5e7e
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-18 17:03:02 +1000

    WORKING SELF TEST

--- src/selftest/SelfTest.cpp
+++ src/selftest/SelfTest.cpp
@@ -21,9 +21,9 @@ float SelfTest::sampleAxis(int pin) {
 
 bool SelfTest::run() {
     Serial.println("[ST] Starting ADXL335 self test...");
-    delay(100);
+    delay(500);
     digitalWrite(_stPin, LOW); // Activate the self test first to fix bug
-    delay(500);  // 500ms delay to stabilise
+    delay(5000);  // 5 second delay to stabilise
 
     // Average readings from the self test
     float stX = sampleAxis(_xPin);
