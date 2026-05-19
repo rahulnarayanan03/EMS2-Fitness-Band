d7018ba2482fc1139273a9bdc46b10d2977f3563
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 08:26:47 +1000

    Implement circle position function

--- src/stopwatch/stopwatch.cpp
+++ src/stopwatch/stopwatch.cpp
@@ -1,2 +1,42 @@
 #include "stopwatch.h"
 
+Stopwatch::Stopwatch(int outer_radius, int thickness, int point_radius) {
+    _outer_radius = outer_radius;
+    _thickness = thickness;
+    _point_radius = point_radius;
+    _period_ms = SW_PERIOD_MS;
+    _elapsed_time = 0;
+}
+
+Stopwatch::Stopwatch(int outer_radius, int thickness, int point_radius, int period_ms) {
+    _outer_radius = outer_radius;
+    _thickness = thickness;
+    _point_radius = point_radius;
+    _period_ms = period_ms;
+    _elapsed_time = 0;
+}
+
+Stopwatch::SW_Time Stopwatch::getElapsedTime() {
+    
+}
+
+float Stopwatch::getElapsedTimeSeconds() {
+    float elapsed_time_f = static_cast<float>(_elapsed_time);
+
+    return elapsed_time_f / 1000.0f;
+}
+
+std::pair<int, int> Stopwatch::getCirclePosition() {
+    std::pair<int, int> coords;
+    int radius = _outer_radius - (_thickness/2);
+
+    float circle_x;
+    float circle_y;
+    circle_x = -1*(float)radius*cos((2000.0f*M_PI/_period_ms)*(getElapsedTimeSeconds() + _period_ms/4000.0f));
+    circle_y = (float)radius*sin((2000.0f*M_PI/_period_ms)*(getElapsedTimeSeconds() + _period_ms/4000.0f));
+
+    coords.first = static_cast<int>(circle_x);
+    coords.second = static_cast<int>(circle_y);
+
+    return coords;
+}
