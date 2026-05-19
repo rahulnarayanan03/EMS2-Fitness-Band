3517e70912285ff7a0bca88c14929feb341305b2
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 08:36:11 +1000

    Isolate getters from setters

--- src/stopwatch/stopwatch.h
+++ src/stopwatch/stopwatch.h
@@ -12,7 +12,7 @@ public:
     // Stopwatch default constructor (no period)
     Stopwatch(int outer_radius, int thickness, int point_radius);
 
-    // Elapsed time formatted in minutes:seconds:milliseconds
+    // Returns elapsed time formatted in minutes:seconds:milliseconds
     // Minutes range from 0-59, seconds range from 0-59, milliseconds range from 0-99 (99 = 999)
     struct SW_Time {
         int milliseconds;
@@ -20,11 +20,14 @@ public:
         int minutes;
     };
 
+    // Updates the x,y coordinates of the circle orbiting the stopwatch
+    void updateCirclePosition();
+
     // Returns the x,y coordinates of the circle orbiting the stopwatch
     std::pair<int, int> getCirclePosition();
 
     // Fills time struct with current elapsed time
-    SW_Time getElapsedTime();
+    SW_Time updateElapsedTime();
 
     // Converts elapsed time to seconds
     float getElapsedTimeSeconds();
@@ -35,5 +38,7 @@ private:
     int _point_radius;  // Radius of the circle orbiting the stopwatch face
     int _period_ms;     // Period of the circle orbiting the stopwatch face
 
-    unsigned long _elapsed_time;
+    unsigned long _elapsed_time;    // Elapsed time in milliseconds
+
+    std::pair<int, int> _point_coords;  // x,y coordinates of the circle orbiting the stopwatch
 };
