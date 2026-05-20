b5bb71da4fc537a2e96d2799ae754155f8055857
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 07:59:18 +1000

    Add constructors and difficulty setting

--- src/game/game.h
+++ src/game/game.h
@@ -11,6 +11,19 @@ public:
         FREE
     };
 
+    // Difficulty level of cpu opponent
+    enum Difficulty {
+        EASY,
+        MEDIUM,
+        HARD
+    };
+
+    // Default constructor
+    Game();
+
+    // Constructor with difficulty level
+    Game(Difficulty difficulty);
+
     // Returns the state of a cell
     // Row and col values range from 1 to 3
     GridState getCellState(int row, int col);
@@ -22,7 +35,14 @@ public:
     // Places an 'X' marker in a cell
     // Row and col values range from 1 to 3
     void placeX(int row, int col);
+
+    // Sets the difficulty of the game
+    void setDifficulty(Difficulty difficulty);
     
 private:
+    // Individual cell states
     GridState game_state[3][3];
+
+    // Difficulty level of the game
+    Difficulty difficulty;
 };
