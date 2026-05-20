40280e2f2bc78b5cdaee2748bc174fcae0278092
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 22:15:03 +1000

    Include pace icon

--- src/display/UI/ui.h
+++ src/display/UI/ui.h
@@ -16,6 +16,7 @@
 #include "../icons/fire.h"
 #include "../icons/axes.h"
 #include "../icons/chip.h"
+#include "../icons/pace.h"
 
 // battery config - swap BATT_ADC_PIN and BATT_DIVIDER_RATIO once PCB schematic confirmed
 static constexpr uint8_t  BATT_ADC_PIN       = 34;
