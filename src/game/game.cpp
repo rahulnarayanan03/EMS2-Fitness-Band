6edb8355e0858813b5af85a1e244d8bf63ba7d01
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 07:55:40 +1000

    Function implementation

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,13 +1,20 @@
 #include "game.h"
 
 Game::GridState Game::getCellState(int row, int col) {
-
+    if (row > 3 || col > 3 || row < 1 || col < 1) {
+        Serial.println("Warning: row/col out of bounds");
+        return Game::FREE;
+    } else {
+        return game_state[row-1][col-1];
+    }
 }
 
 void Game::placeO(int row, int col) {
-
+    game_state[row-1][col-1] = Game::OCCUPIED_O;
+    return;
 }
 
 void Game::placeX(int row, int col) {
-
+    game_state[row-1][col-1] = Game::OCCUPIED_X;
+    return;
 }
