619a784a43b91a264f6f076bd78203d527f38ffc
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 07:52:01 +1000

    Specify row/col convention

--- src/game/game.h
+++ src/game/game.h
@@ -10,12 +10,15 @@ public:
     };
 
     // Returns the state of a cell
+    // Row and col values range from 1 to 3
     GridState getCellState(int row, int col);
 
     // Places an 'O' marker in a cell
+    // Row and col values range from 1 to 3
     void placeO(int row, int col);
 
     // Places an 'X' marker in a cell
+    // Row and col values range from 1 to 3
     void placeX(int row, int col);
     
 private:
