965ca22a3be47c8c3fa732943201e1e1de7e6034
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 10:35:16 +1000

    Implement stopwatch start

--- src/stopwatch/stopwatch.cpp
+++ src/stopwatch/stopwatch.cpp
@@ -55,3 +55,13 @@ float Stopwatch::getElapsedTimeSeconds() {
 Stopwatch::SW_Time Stopwatch::getFormattedTime() {
     return _formatted_time;
 }
+
+void Stopwatch::start_sw() {
+    unsigned long t1 = millis();
+
+    delay(SW_DELAY);
+
+    unsigned long t2 = millis();
+
+    _elapsed_time += t2 - t1;
+}
