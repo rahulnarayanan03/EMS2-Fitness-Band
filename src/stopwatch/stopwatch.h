aa28db66718b4aa356aed4ede1952a7ece2d7337
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 11:48:27 +1000

    State setter

--- src/stopwatch/stopwatch.h
+++ src/stopwatch/stopwatch.h
@@ -48,6 +48,8 @@ public:
     // Returns elapsed time formatted in minutes:seconds:milliseconds
     SW_Time getFormattedTime();
 
+    void setState(SW_State state);
+
     // Returns state of the stopwatch
     SW_State getState();
 
