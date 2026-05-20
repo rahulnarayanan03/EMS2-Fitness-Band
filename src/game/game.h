bb7e1d40f7b21cd0d441a7567b3d7adbcc8e5965
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 07:46:52 +1000

    tic tac toe start

--- src/game/game.h
+++ src/game/game.h
@@ -1,4 +1,15 @@
 class Game {
 public:
+    // State of each grid
+    enum GridState {
+        OCCUPIED_O,
+        OCCUPIED_X,
+        FREE
+    };
+
+    // Returns true if a cell is occupied, false if it is free
+    bool getOccupied(int row, int col);
     
+private:
+    GridState game_state[3][3];
 };
