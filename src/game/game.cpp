8a238912be517b0935feb65b7a008fb51b936966
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 08:04:39 +1000

    Add function to reset the grid

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,5 +1,13 @@
 #include "game.h"
 
+Game::Game() {
+
+}
+
+Game::Game(Game::Difficulty difficulty) {
+    resetGrid();
+}
+
 Game::GridState Game::getCellState(int row, int col) {
     if (row > 3 || col > 3 || row < 1 || col < 1) {
         Serial.println("Warning: row/col out of bounds");
@@ -17,4 +25,12 @@ void Game::placeO(int row, int col) {
 void Game::placeX(int row, int col) {
     game_state[row-1][col-1] = Game::OCCUPIED_X;
     return;
+}
+
+void Game::resetGrid() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            game_state[i][j] = Game::FREE;
+        }
+    }
 }
