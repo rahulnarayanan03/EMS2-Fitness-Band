592609680fef93c40b8f19b5b591e641f477a6d5
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-21 07:48:08 +1000

    Placement functions

--- src/game/game.h
+++ src/game/game.h
@@ -9,6 +9,12 @@ public:
 
     // Returns true if a cell is occupied, false if it is free
     bool getOccupied(int row, int col);
+
+    // Places an 'O' marker in a cell
+    void placeO(int row, int col);
+
+    // Places an 'X' marker in a cell
+    void placeX(int row, int col);
     
 private:
     GridState game_state[3][3];
