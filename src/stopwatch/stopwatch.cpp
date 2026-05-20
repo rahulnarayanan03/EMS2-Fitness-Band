3cd0ddd0d014430f21f530361c3edce174601583
leoo-c1 <chinchillaleo01@gmail.com>
2026-05-20 11:48:34 +1000

    State setter implementation

--- src/stopwatch/stopwatch.cpp
+++ src/stopwatch/stopwatch.cpp
@@ -67,6 +67,10 @@ Stopwatch::SW_Time Stopwatch::getFormattedTime() {
     return _formatted_time;
 }
 
+void Stopwatch::setState(SW_State state) {
+    _state = state;
+}
+
 Stopwatch::SW_State Stopwatch::getState() {
     return _state;
 }
